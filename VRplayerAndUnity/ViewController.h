6dd1a63f61baff9171c957fec6cf60f196309fb0
郑锦斌 <533335@nd.com>
2016-06-21 11:39:47 +0800

    -Modified
    初始化

---
+++ VRplayerAndUnity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VRplayerAndUnity
+//
+//  Created by admin on 16/6/21.
+//  Copyright © 2016年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
